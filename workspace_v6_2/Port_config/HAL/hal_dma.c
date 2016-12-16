3e342152da3e0fa615b0eec05c7092891ad71823
DESKTOP-0G0FEV3\Nikolaus Lacher <nikolaus.lacher@edu.fh-joanneum.at>
2016-12-16 12:06:42 +0100

    ak

---
+++ workspace_v6_2/Port_config/HAL/hal_dma.c
@@ -0,0 +1,13 @@
+/*
+ * hal_dma.c
+ *
+ *  Created on: 13. Dez. 2016
+ *      Author: Nikolaus Lacher
+ */
+
+
+
+void HAL_DMA_Init()
+{
+	DAMCTL0 |=
+}
---
+++ workspace_v6_2/Port_config/HAL/hal_dma.h
@@ -0,0 +1,15 @@
+/*
+ * hal_dma.h
+ *
+ *  Created on: 13. Dez. 2016
+ *      Author: Nikolaus Lacher
+ */
+
+#ifndef HAL_HAL_DMA_H_
+#define HAL_HAL_DMA_H_
+
+void HAL_DMA_Init(void);
+
+
+
+#endif /* HAL_HAL_DMA_H_ */
